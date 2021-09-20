2a5c30523c5b0c5d66d46ddd6e029f27249e837d
muahmmadfarhan <m.farhankz@gmail.com>
2021-09-20 16:53:47 +0500

    path changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,14 @@ import { TabMenuModule } from 'primeng/tabmenu';
 import { TabViewModule } from 'primeng/tabview';
 import { DividerModule } from 'primeng/divider';
 import { RippleModule } from "primeng/ripple";
-import { MenuComponent } from './shared/menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
 import { DropdownModule } from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import {CardModule} from 'primeng/card';
-import { SplitterComponent } from './shared/splitter/splitter.component';
-import { SplitterLeftComponent } from './shared/splitter-left/splitter-left.component';
-import { SplitterRightComponent } from './shared/splitter-right/splitter-right.component';
+import { SplitterComponent } from './components/splitter/splitter.component';
+import { SplitterLeftComponent } from './components/splitter-left/splitter-left.component';
+import { SplitterRightComponent } from './components/splitter-right/splitter-right.component';
 
 
 
